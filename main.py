8ce6fb82eea67f65dd367b31a4bfce5c92066ddb
Liam <143455729+qbyz@users.noreply.github.com>
2025-03-04 10:09:39 -0500

    Finished Input Handling, Started Solver

--- main.py
+++ main.py
@@ -19,29 +19,36 @@ def inputHandling(firstEquation):
 
     # Parse Reactant List into Dictionary with Values
     for x in range(len(reactantSplit)):
+        skip_next = False
         for y in range(len(reactantSplit[x])):
+            if skip_next:
+                skip_next = False
+                continue
 
             # If there is more than one of an atom in the compound
             if '^' in reactantSplit[x]:
-
                 if y > 0 and y < len(reactantSplit[x]):
                     if reactantSplit[x][y] == '^':
-                        # Remove the check for isupper() on the ^ character
-                        if y > 1:
+                        # Check if character before ^ is uppercase (single element) or lowercase (part of two-letter element)
+                        if reactantSplit[x][y - 1].islower() and y > 1:
+                            # Two-letter element (like 'Cl' in 'H^2Cl')
                             reactantDict.update(
                                 {f'{reactantSplit[x][y - 2]}{reactantSplit[x][y - 1]}': int(reactantSplit[x][y + 1])})
                         else:
+                            # Single letter element
                             reactantDict.update({reactantSplit[x][y - 1]: int(reactantSplit[x][y + 1])})
 
-            else:
-                if y < len(reactantSplit[x]) - 1:  # Fixed boundary check
-                    if reactantSplit[x][y].isupper() and reactantSplit[x][y+1].islower():
-                        reactantDict.update(
-                            {f'{reactantSplit[x][y]}{reactantSplit[x][y+1]}': 1})
-                        reactantSplit[x][y+1] = '_'
-                    elif reactantSplit[x][y].isupper():
-                        reactantDict.update(
-                            {reactantSplit[x][y]: 1})
+            # Handle elements without subscripts
+            if y < len(reactantSplit[x]) and reactantSplit[x][y].isupper():
+                # Check if this is followed by a lowercase letter (two-letter element)
+                if y + 1 < len(reactantSplit[x]) and reactantSplit[x][y + 1].islower():
+                    # Check if this is already processed by ^ handling
+                    if y + 2 >= len(reactantSplit[x]) or reactantSplit[x][y + 2] != '^':
+                        reactantDict.update({f'{reactantSplit[x][y]}{reactantSplit[x][y + 1]}': 1})
+                        skip_next = True
+                # Single letter element not followed by ^
+                elif y + 1 >= len(reactantSplit[x]) or reactantSplit[x][y + 1] != '^':
+                    reactantDict.update({reactantSplit[x][y]: 1})
 
     for i in range(len(products)):
         # Remove spaces first, then convert to list
@@ -49,29 +56,52 @@ def inputHandling(firstEquation):
 
     productDict = dict()
 
+    # Using the same improved logic for products
     for x in range(len(productSplit)):
+        skip_next = False
         for y in range(len(productSplit[x])):
+            if skip_next:
+                skip_next = False
+                continue
+
+            # If there is more than one of an atom in the compound
             if '^' in productSplit[x]:
                 if y > 0 and y < len(productSplit[x]):
                     if productSplit[x][y] == '^':
-                        if productSplit[x][y-1].isupper():
+                        # Check if character before ^ is uppercase (single element) or lowercase (part of two-letter element)
+                        if productSplit[x][y - 1].islower() and y > 1:
+                            # Two-letter element (like 'Cl' in 'ZnCl^2')
+                            productDict.update(
+                                {f'{productSplit[x][y - 2]}{productSplit[x][y - 1]}': int(productSplit[x][y + 1])})
+                        else:
+                            # Single letter element
                             productDict.update({productSplit[x][y - 1]: int(productSplit[x][y + 1])})
 
-                        else:
-                            productDict.update({f'{productSplit[x][y-2]}{productSplit[x][y-1]}': int(productSplit[x][y + 1])})
-                elif productSplit[x][y].isupper() and productSplit[x][y + 1].islower():
-                    productDict.update({f'{productSplit[x][y]}{productSplit[x][y + 1]}': 1})
+            # Handle elements without subscripts
+            if y < len(productSplit[x]) and productSplit[x][y].isupper():
+                # Check if this is followed by a lowercase letter (two-letter element)
+                if y + 1 < len(productSplit[x]) and productSplit[x][y + 1].islower():
+                    # Check if this is already processed by ^ handling
+                    if y + 2 >= len(productSplit[x]) or productSplit[x][y + 2] != '^':
+                        productDict.update({f'{productSplit[x][y]}{productSplit[x][y + 1]}': 1})
+                        skip_next = True
+                # Single letter element not followed by ^
+                elif y + 1 >= len(productSplit[x]) or productSplit[x][y + 1] != '^':
+                    productDict.update({productSplit[x][y]: 1})
 
-            else:
-                if y < len(productSplit[x]) - 1:  # Fixed boundary check
-                    if productSplit[x][y].isupper() and productSplit[x][y + 1].islower():
-                        productDict.update(
-                            {f'{productSplit[x][y]}{productSplit[x][y + 1]}': 1})
-                        productSplit[x][y + 1] = '_'
-                    elif productSplit[x][y].isupper():
-                        productDict.update(
-                            {productSplit[x][y]: 1})
     return reactantDict, productDict
 
 
+def solver(reactDic, prodDic, reactParts, prodParts):
+    values = dict()
+    letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+               'V', 'W', 'X', 'Y', 'Z'}
+
+    for x in reactDic:
+        if x == prodDic[x]:
+            print('yay')
+    #for i in prodParts:
+        #values.update({letters[i]:1})
+
+solver({'C': 1, 'O': 2}, {'C': 1, 'O': 2}, ['x'], ['y'])
 print(inputHandling(ogString))
