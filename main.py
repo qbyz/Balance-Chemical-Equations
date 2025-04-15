2bade258d381121ceb0973e7227d8f3a6affa1e0
Liam <143455729+qbyz@users.noreply.github.com>
2025-04-15 10:08:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def oneSide(side):
         actStart = i
         sideConsts.append(const)
 
-        print(f"Component: {comp}, Constant: {const}")
+        #print(f"Component: {comp}, Constant: {const}")
 
         if '(' in comp and ')' in comp:
             i = comp.index(')') + 2
@@ -89,7 +89,7 @@ def oneSide(side):
                     # If 1 letter, in dictionary already, and there are no ^s
                     elif x < len(comp)-1 and comp[x].isupper() and comp[x] in sideVals[counter] and comp[x+2] != '^':
                         existing = int(sideVals[counter].get(comp[x]))
-                        sideVals[counter].update({[comp[x]] : const + existing})
+                        sideVals[counter].update({comp[x] : const + existing})
 
                     # If 1 letter, not in dictionary already, and there are no ^s
                     elif x < len(comp)-1 and comp[x].isupper() and comp[x+2] != '^':
@@ -176,6 +176,7 @@ def oneSide(side):
                         multiple = 1
                     else:
                         multiple = int(multiple)
+                    print(f'HI {multiple}')
 
                 else:
                     i = x + 2
@@ -187,52 +188,52 @@ def oneSide(side):
                         multiple = 1
                     else:
                         multiple = int(multiple)
+                print(f'MULTIPLE {multiple}')
+                if len(comp) == 1:
+                    sideVals[counter][comp[x]] = const
 
-                    if len(comp) == 1:
-                        sideVals[counter][comp[x]] = const
+                # If 2 letter, in dictionary already, and there are no ^s
+                elif x < len(comp)-1 and comp[x].isupper() and comp[x+1].islower() and f'{comp[x]}{comp[x+1]}' in sideVals[counter] and comp[x+2] != '^':
+                    existing = int(sideVals[counter].get(f'{comp[x]}{comp[x+1]}'))
+                    sideVals[counter].update({f'{comp[x]}{comp[x+1]}': existing + const})
 
-                    # If 2 letter, in dictionary already, and there are no ^s
-                    elif x < len(comp)-1 and comp[x].isupper() and comp[x+1].islower() and f'{comp[x]}{comp[x+1]}' in sideVals[counter] and comp[x+2] != '^':
-                        existing = int(sideVals[counter].get(f'{comp[x]}{comp[x+1]}'))
-                        sideVals[counter].update({f'{comp[x]}{comp[x+1]}': existing + const})
+                # If 2 letter, not in dictionary already, and there are no ^s
+                elif x < len(comp)-1 and comp[x].isupper() and comp[x+1].islower() and comp[x+2] != '^':
+                    sideVals[counter][f'{comp[x]}{comp[x + 1]}'] = const
 
-                    # If 2 letter, not in dictionary already, and there are no ^s
-                    elif x < len(comp)-1 and comp[x].isupper() and comp[x+1].islower() and comp[x+2] != '^':
-                        sideVals[counter][f'{comp[x]}{comp[x + 1]}'] = const
+                # If 1 letter, in dictionary already, and there are no ^s
+                elif x < len(comp)-1 and comp[x].isupper() and comp[x] in sideVals[counter] and comp[x+1] != '^':
+                    existing = int(sideVals[counter].get(comp[x]))
+                    sideVals[counter].update({comp[x] : const + existing})
 
-                    # If 1 letter, in dictionary already, and there are no ^s
-                    elif x < len(comp)-1 and comp[x].isupper() and comp[x] in sideVals[counter] and comp[x+1] != '^':
-                        existing = int(sideVals[counter].get(comp[x]))
-                        sideVals[counter].update({[comp[x]] : const + existing})
+                # If 1 letter, not in dictionary already, and there are no ^s
+                elif x < len(comp)-1 and comp[x].isupper() and comp[x+1] != '^':
+                    sideVals[counter][comp[x]] = const
 
-                    # If 1 letter, not in dictionary already, and there are no ^s
-                    elif x < len(comp)-1 and comp[x].isupper() and comp[x+1] != '^':
-                        sideVals[counter][comp[x]] = const
+                # Start of ^s
 
-                    # Start of ^s
+                # If 2 letter, in dictionary already, and there are ^s
+                elif x < len(comp) - 1 and comp[x].isupper() and comp[x + 1].islower() and f'{comp[x]}{comp[x + 1]}' in sideVals[counter] and comp[x+2] == '^':
+                    existing = int(sideVals[counter].get(f'{comp[x]}{comp[x + 1]}'))
+                    sideVals[counter].update({f'{comp[x]}{comp[x + 1]}': existing + (const * multiple)})
 
-                    # If 2 letter, in dictionary already, and there are ^s
-                    elif x < len(comp) - 1 and comp[x].isupper() and comp[x + 1].islower() and f'{comp[x]}{comp[x + 1]}' in sideVals[counter] and comp[x+2] == '^':
-                        existing = int(sideVals[counter].get(f'{comp[x]}{comp[x + 1]}'))
-                        sideVals[counter].update({f'{comp[x]}{comp[x + 1]}': existing + (const * multiple)})
+                # If 2 letter, not in dictionary already, and there are ^s
+                elif x < len(comp) - 1 and comp[x].isupper() and comp[x + 1].islower() and comp[x+2] == '^':
+                    sideVals[counter][comp[x]+comp[x + 1]] = const * multiple
 
-                    # If 2 letter, not in dictionary already, and there are ^s
-                    elif x < len(comp) - 1 and comp[x].isupper() and comp[x + 1].islower() and comp[x+2] == '^':
-                        sideVals[counter][comp[x]+comp[x + 1]] = const * multiple
+                # If 1 letter, in dictionary already, and there are ^s
+                elif x < len(comp) - 1 and comp[x].isupper() and comp[x] in sideVals[counter] and comp[x+1] == '^':
+                    existing = int(sideVals[counter].get(comp[x]))
+                    sideVals[counter].update({comp[x]: (const * multiple) + existing})
 
-                    # If 1 letter, in dictionary already, and there are ^s
-                    elif x < len(comp) - 1 and comp[x].isupper() and comp[x] in sideVals[counter] and comp[x+1] == '^':
-                        existing = int(sideVals[counter].get(comp[x]))
-                        sideVals[counter].update({comp[x]: (const * multiple) + existing})
-
-                    # If 1 letter, not in dictionary already, and there are ^s
-                    elif x < len(comp) - 1 and comp[x].isupper() and comp[x+1] == '^':
-                        sideVals[counter][comp[x]] = const * multiple
+                # If 1 letter, not in dictionary already, and there are ^s
+                elif x < len(comp) - 1 and comp[x].isupper() and comp[x+1] == '^':
+                    sideVals[counter][comp[x]] = const * multiple
         counter += 1
-
+    print(f'Dictionary: {sideVals}')
     return sideVals
 def mergeDicts(dicts):
-    print('Merging...')
+    #print('Merging...')
     result = dict()
     for i in dicts:
         for j in i:
@@ -263,13 +264,14 @@ def createEquation(reacts, prods):
                 vectList[j].append(int(i.get(elementList[j])))
             else:
                 vectList[j].append(0)
+    print(f'Vectors: {vectList}')
     return vectList
 
 
 def createEquation(reacts, prods):
     # Combine reactant and product dictionaries into one list; order is preserved.
     compoundList = reacts + prods
-
+    print(f'Combined: {compoundList}')
     # Get the union (merged dictionary) of all compound dictionaries.
     # Then extract the keys (unique element symbols) in a list.
     uniqueElements = list(mergeDicts(compoundList).keys())
@@ -291,7 +293,13 @@ def solver(eqs):
 
     nullSpace = matrix.nullspace()
 
+    if not nullSpace:
+        return False
     solution = nullSpace[0]
+    i=1
+    while 0 in solution and i < len(nullSpace):
+        solution = nullSpace[i]
+        i+=1
 
     lcmList = lcm([i.q for i in solution])
 
@@ -301,13 +309,17 @@ def solver(eqs):
 
 # Example usage:
 matrix, compoundList = createEquation(oneSide(reactants), oneSide(products))
-print("Compound List:", compoundList)
-print(f'split value:{len(oneSide(reactants))-1}')
-print(matrix[len(oneSide(reactants))-1])
+#print("Compound List:", compoundList)
+#print(f'split value:{len(oneSide(reactants))-1}')
+#print(matrix[len(oneSide(reactants))-1])
 i = len(oneSide(reactants))
+print(f'I Value: {i}')
 while i < len(matrix):
+    print(f'Matrix: {matrix[i]}')
     for j in range(len(matrix[i])):
         matrix[i][j] *= -1
     print(f'products: {matrix[i]}')
     i+=1
+print(f'Final Matrix: {matrix}')
 print(solver(matrix))
+print(f'TEST!!!!! {oneSide(["Mg^4"])}')
