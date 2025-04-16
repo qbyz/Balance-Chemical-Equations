91a95feaf7a08bbb449ec89daa9b33f95a93184e
Liam <143455729+qbyz@users.noreply.github.com>
2025-04-16 10:57:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ sides = ogString.split('=')
 reactants = sides[0].split('+')
 products = sides[1].split('+')
 
+
 def oneSide(side):
     sideSplit = []
     sideVals = []
+
     for i in range(len(side)):
         # Remove spaces first, then convert to list
         sideSplit.append(list(side[i].replace(" ", "")))
@@ -18,39 +20,101 @@ def oneSide(side):
     for counter, comp in enumerate(sideSplit):
         i = 0
         const = 1  # Default constant if none provided
+        stack = []  # Stack to handle brackets
+        temp_dict = {}  # Temporary dictionary for parsed elements
+
         while i < len(comp):
-            if comp[i].isdigit():
-                # Handle leading coefficients (e.g., "2Mg^4")
-                const = int(comp[i])
+            if comp[i] == '(':
+                polyco = True
+                # Push current temp_dict and const to stack
+                stack.append((temp_dict, const))
+                temp_dict = {}
+                const = 1
                 i += 1
-            elif comp[i].isupper():
-                # Handle elements (e.g., "Mg")
+            elif comp[i] == ')':
+                # Process the closing bracket
+                i += 1
+                multiplier = ''
+                while i < len(comp) and comp[i].isdigit():
+                    multiplier += comp[i]
+                    i += 1
+                multiplier = int(multiplier) if multiplier else 1
+                # Multiply elements inside brackets by the multiplier
+                for key in temp_dict:
+                    temp_dict[key] *= multiplier
+                # Pop the previous dictionary and merge
+                prev_dict, prev_const = stack.pop()
+                for key, value in temp_dict.items():
+                    if key in prev_dict:
+                        prev_dict[key] += value
+                    else:
+                        prev_dict[key] = value
+                temp_dict = prev_dict
+                const = prev_const
+            elif comp[i].isdigit():
+                # Handle leading coefficients (e.g., "2Mg^4")
+                const = ''
+                while i < len(comp) and comp[i].isdigit():
+                    const += comp[i]
+                    i += 1
+                const = int(const)
+            elif comp[i].isupper() and not polyco:
+                # Parse elements
                 element = comp[i]
                 if i + 1 < len(comp) and comp[i + 1].islower():
                     element += comp[i + 1]
                     i += 1
                 i += 1
-                # Handle subscripts (e.g., "^4")
+                # Parse subscripts
+                subscript = ''
                 if i < len(comp) and comp[i] == '^':
                     i += 1
-                    subscript = ''
                     while i < len(comp) and comp[i].isdigit():
                         subscript += comp[i]
                         i += 1
                     subscript = int(subscript)
                 else:
                     subscript = 1
-                # Update dictionary with element and its count
-                if element in sideVals[counter]:
-                    sideVals[counter][element] += const * subscript
+                # Update the temporary dictionary
+                if element in temp_dict:
+                    temp_dict[element] += const * subscript
                 else:
-                    sideVals[counter][element] = const * subscript
+                    temp_dict[element] = const * subscript
+                    
+            elif comp[i].isupper() and polyco:
+                # Parse elements
+                element = comp[i]
+                if i + 1 < len(comp) and comp[i + 1].islower():
+                    element += comp[i + 1]
+                    i += 1
+                i += 1
+                # Parse subscripts
+                subscript = ''
+                if i < len(comp) and comp[i] == '^':
+                    i += 1
+                    while i < len(comp) and comp[i].isdigit():
+                        subscript += comp[i]
+                        i += 1
+                    subscript = int(subscript)
+                else:
+                    subscript = 1
+                # Update the temporary dictionary
+                if element in temp_dict:
+                    temp_dict[element] += const * subscript
+                else:
+                    temp_dict[element] = const * subscript
             else:
-                i += 1  # Skip invalid characters (if any)
-    return sideValsdef 
+                # Move to the next character
+                i += 1
 
-mergeDicts(dicts):
-    #print('Merging...')
+        # Merge the parsed dictionary into sideVals
+        sideVals[counter] = temp_dict
+
+    return sideVals
+
+
+def mergeDicts(dicts):
+    # print('Merging...')
     result = dict()
     for i in dicts:
         for j in i:
@@ -61,14 +125,15 @@ mergeDicts(dicts):
 
     return result
 
-def solveCheck(check1, check2):
 
+def solveCheck(check1, check2):
     print('Checking')
     if check1 == check2:
         return True
     else:
         return False
-        
+
+
 def createEquation(reacts, prods):
     reactElements = mergeDicts(reacts)
     prodElements = mergeDicts(prods)
@@ -106,6 +171,7 @@ def createEquation(reacts, prods):
 
     return equations, compoundList
 
+
 def solver(eqs):
     matrix = Matrix(eqs).transpose()
 
@@ -114,10 +180,10 @@ def solver(eqs):
     if not nullSpace:
         return False
     solution = nullSpace[0]
-    i=1
+    i = 1
     while 0 in solution and i < len(nullSpace):
         solution = nullSpace[i]
-        i+=1
+        i += 1
 
     lcmList = lcm([i.q for i in solution])
 
@@ -125,11 +191,12 @@ def solver(eqs):
 
     return solution
 
+
 # Example usage:
 matrix, compoundList = createEquation(oneSide(reactants), oneSide(products))
-#print("Compound List:", compoundList)
-#print(f'split value:{len(oneSide(reactants))-1}')
-#print(matrix[len(oneSide(reactants))-1])
+# print("Compound List:", compoundList)
+# print(f'split value:{len(oneSide(reactants))-1}')
+# print(matrix[len(oneSide(reactants))-1])
 i = len(oneSide(reactants))
 print(f'I Value: {i}')
 while i < len(matrix):
@@ -137,7 +204,6 @@ while i < len(matrix):
     for j in range(len(matrix[i])):
         matrix[i][j] *= -1
     print(f'products: {matrix[i]}')
-    i+=1
+    i += 1
 print(f'Final Matrix: {matrix}')
 print(solver(matrix))
-print(f'TEST!!!!! {oneSide(["Mg^4"])}')
